8e275e0bc0601f343fbd346f287f5de8820d4849
SCARLATT27122015 <mansion.lagrimas@gmail.com>
2021-04-28 19:56:26 -0500

    add custom validation hook

--- src/components/FormSearch.js
+++ src/components/FormSearch.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 
-const FormSearch = () => {
+const initialForm = {
+    artist:"",
+    song:""
+}
+
+const FormSearch = ({handleSearch}) => {
+    const [form, setForm] = useState(initialForm);
+
+    const handleChange=e=>{
+        const {name, value} = e.target
+        setForm({
+            ...form, [name]:value
+        });
+    };
+
+    const handleSubmit = e => {
+        e.preventDefault();
+        handleSearch(form);
+
+    }
+
     return (
         <div>
-            <form>
-                <input type="text"/>
-                <input type="text"/>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={form.artist} onChange={handleChange} name="artist" placeholder="Artist name"/>
+                <input type="text" value={form.song} onChange={handleChange} name="song" placeholder="Song name"/>
+                <input type="submit" value="Search"/>
             </form>
         </div>
     )
--- src/components/SongSearcher.js
+++ src/components/SongSearcher.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Details from './Details'
 import FormSearch from './FormSearch';
 import Load from './Load'
@@ -6,13 +6,50 @@ import Load from './Load'
 
 const SongSearcher = () => {
     const [load, setLoad] = useState(false);
+    const [bio, setBio] = useState(null);
+    const [lyrics, setLyrics] = useState(null);
+    const [search, setSearch] = useState(null);
 
+    const handleSearch = data => {
+        setSearch(data);
+    };
+
+    useEffect(() => {
+        if (!search) return;
+
+
+        const getData = async (search) =>{
+            const options = {
+                'Content-Type':'application/json'
+            };
+
+            const {artist,song} = search;
+            let urlArtist = `https://theaudiodb.com/api/v1/json/1/search.php?s=${artist}`,
+            urlLyrics = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+            
+            try {
+                const [arrArtist, arrLyrics] = await Promise.all([fetch(urlArtist, options),fetch(urlLyrics, options)]);
+
+                if (!arrArtist.ok || !arrLyrics.ok) throw new Error('There was an error on the requests');
+
+                const jsonArtist = await arrArtist.json();
+                const jsonLyrics = await arrLyrics.json();
+                
+                console.log(jsonLyrics);
+                console.log(jsonArtist);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        getData(search);
+    }, [search]);
 
     return (
         <div>
             <h2>Song searcher</h2>
-            <Load/>
-            <FormSearch/>
+            {load && <Load/>}
+            <FormSearch handleSearch={handleSearch}/>
             <Details/>
         </div>
     )
